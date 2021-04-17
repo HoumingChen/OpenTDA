7590c23f169319f606d6476ae539f3d884968612
HoumingChen <54627930+HoumingChen@users.noreply.github.com>
2021-04-17 16:44:01 -0400

    Update persistent_homology.py

--- tda/persistent_homology.py
+++ tda/persistent_homology.py
@@ -4,8 +4,49 @@ import functools
 import numpy as np
 import networkx as nx
 from scipy.spatial.distance import squareform, pdist
+import matplotlib.pyplot as plt
 
-from .snf import low, smith_normal_form
+
+
+
+#returns row index of lowest "1" in a column i in the boundary matrix
+def low(i, matrix):
+    col = matrix[:,i]
+    col_len = len(col)
+    for i in range( (col_len-1) , -1, -1): #loop through column from bottom until you find the first 1
+        if col[i] == 1: return i
+    return -1 #if no lowest 1 (e.g. column of all zeros), return -1 to be 'undefined'
+
+#checks if the boundary matrix is fully reduced
+def isReduced(matrix):
+    for j in range(matrix.shape[1]): #iterate through columns
+        for i in range(j): #iterate through columns before column j
+            low_j = low(j, matrix)
+            low_i = low(i, matrix)
+            if (low_j == low_i and low_j != -1):
+                return i,j #return column i to add to column j
+    return [0,0]
+
+#the main function to iteratively reduce the boundary matrix
+def reduceBoundaryMatrix(matrix): 
+    #this refers to column index in the boundary matrix
+    reduced_matrix = matrix.copy()
+    matrix_shape = reduced_matrix.shape
+    memory = np.identity(matrix_shape[1], dtype='>i8') #this matrix will store the column additions we make
+    r = isReduced(reduced_matrix)
+    while (r != [0,0]):
+        i = r[0]
+        j = r[1]
+        col_j = reduced_matrix[:,j]
+        col_i = reduced_matrix[:,i]
+        print("Mod: add col %s to %s \n" % (i+1,j+1)) #Uncomment to see what mods are made
+        reduced_matrix[:,j] = np.bitwise_xor(col_i,col_j) #add column i to j
+        memory[i,j] = 1
+        r = isReduced(reduced_matrix)
+    return reduced_matrix, memory
+
+
+
 
 __all__ = ['PersistentHomology']
 
@@ -137,39 +178,59 @@ def filterBoundaryMatrix(filterComplex):
     return bmatrix
 
 
-def readIntervals(reduced_matrix, filterValues):
+def readIntervals(reduced_matrix, filterValues): #reduced_matrix includes the reduced boundary matrix AND the memory matrix
+    #store intervals as a list of 2-element lists, e.g. [2,4] = start at "time" point 2, end at "time" point 4
+    #note the "time" points are actually just the simplex index number for now. we will convert to epsilon value later
     intervals = []
-    m = reduced_matrix.shape[1]
-    for j in range(m):
-        low_j = low(j, reduced_matrix)
-        if low_j == (m - 1):
+    #loop through each column j
+    #if low(j) = -1 (undefined, all zeros) then j signifies the birth of a new feature j
+    #if low(j) = i (defined), then j signifies the death of feature i
+    for j in range(reduced_matrix[0].shape[1]): #for each column (its a square matrix so doesn't matter...)
+        low_j = low(j, reduced_matrix[0])
+        if low_j == -1:
             interval_start = [j, -1]
-            intervals.append(interval_start)
-
-        else:
-            feature = intervals.index([low_j, -1])
-            intervals[feature][1] = j
+            intervals.append(interval_start) # -1 is a temporary placeholder until we update with death time
+            #if no death time, then -1 signifies feature has no end (start -> infinity)
+            #-1 turns out to be very useful because in python if we access the list x[-1] then that will return the
+            #last element in that list. in effect if we leave the end point of an interval to be -1
+            # then we're saying the feature lasts until the very end
+        else: #death of feature
+            feature = intervals.index([low_j, -1]) #find the feature [start,end] so we can update the end point
+            intervals[feature][1] = j #j is the death point
+            #if the interval start point and end point are the same, then this feature begins and dies instantly
+            #so it is a useless interval and we dont want to waste memory keeping it
             epsilon_start = filterValues[intervals[feature][0]]
             epsilon_end = filterValues[j]
-            if epsilon_start == epsilon_end:
-                intervals.remove(intervals[feature])
-
+            if epsilon_start == epsilon_end: intervals.remove(intervals[feature])
+            
     return intervals
 
-
-def readPersistence(intervals, filterComplex):
+def readPersistence(intervals, filterComplex): 
+    #this converts intervals into epsilon format and figures out which homology group each interval belongs to
     persistence = []
     for interval in intervals:
         start = interval[0]
         end = interval[1]
-
-        homology_group = (len(filterComplex[0][start]) - 1)
+        homology_group = (len(filterComplex[0][start]) - 1) #filterComplex is a list of lists [complex, filter values]
         epsilon_start = filterComplex[1][start]
         epsilon_end = filterComplex[1][end]
         persistence.append([homology_group, [epsilon_start, epsilon_end]])
-
+        
     return persistence
 
+def graph_barcode(persistence, homology_group = 0): 
+    #this function just produces the barcode graph for each homology group
+    xstart = [s[1][0] for s in persistence if s[0] == homology_group]
+    xstop = [s[1][1] for s in persistence if s[0] == homology_group]
+    y = [0.1 * x + 0.1 for x in range(len(xstart))]
+    plt.hlines(y, xstart, xstop, color='b', lw=4)
+    #Setup the plot
+    ax = plt.gca()
+    plt.ylim(0,max(y)+0.1)
+    ax.yaxis.set_major_formatter(plt.NullFormatter())
+    plt.xlabel('epsilon')
+    plt.ylabel("Betti dim %s" % (homology_group,))
+    plt.show()
 
 class PersistentHomology(object):
 
